88828e899bbc5bcff8ece57f6cb7fae2accd3044
jangtjdgns <jangtjdgns0208@gmail.com>
2023-08-07 21:11:42 +0900

    연혁. 틀 작성

--- js/mainNotice.js
+++ js/mainNotice.js
@@ -1,6 +1,7 @@
 $(function () {
     var textBtn = $('.main-text-btn');
     textBtn.eq(2).hide(); // 시작 버튼 숨기기
+
     var textY = 0;
     var textTlY = ($('.text').length - 1) * -100;       // text 수 * 100 -> translateY
     var repeatNotice = setInterval(noticeDown, 5000);   // 5초마다 반복
@@ -10,6 +11,7 @@ $(function () {
         buttonCheckControl($(this).attr('id'));
     });
 
+    // 누른 버튼 확인하는 함수
     function buttonCheckControl(btn) {
         if (btn == 'notice-up') {
             return noticeUp();
@@ -25,16 +27,19 @@ $(function () {
         return clearInterval(repeatNotice);
     }
 
+    // 업 버튼 클릭 시
     function noticeUp() {
         textY < 0 ? textY += 100 : textY = textTlY;
         return setTrnaslateY();
     }
 
+    // 다운 버튼 클릭 시
     function noticeDown() {
         textY > textTlY ? textY -= 100 : textY = 0;
         return setTrnaslateY();
     }
 
+    // text클래스 트렌스폼 적용
     function setTrnaslateY() {
         $('.text').each(function () {
             $(this).css("transform", "translateY(" + textY + "%)");
